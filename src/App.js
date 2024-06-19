d8c6a5f4ca66827a01ca5042e033a43b9fadbd70
thanhson <steaklord1307@gmail.com>
2024-06-19 12:23:40 +0700

    added cool css title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './images/ape.png';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(()=>{
+  function makeTextCool(id){
+      var element = document.getElementById(id);
+      if(element){
+        var text = element.innerText.split("");
+        element.innerText = "";
+        text.forEach((letter) => {
+          var span = document.createElement('span');
+          span.innerText = letter;
+          span.classList.add('letter');
+          element.appendChild(span);
+        })
+      }
+    }
+    makeTextCool('title');
+    }
+  )
+  
   return (
     <div className="App">
       <div id='navbar-ctn'>
@@ -13,7 +32,16 @@ function App() {
         </ul>
         <a className='cta' href='#'><button>Contact</button></a>
       </div>
-
+      <div id='title' className='fancy' >BasicWeb</div>
+      {/* <div id='products'>
+        <div className='product-card'>
+          <div className='p-i-ctn'>
+            <img></img>
+          </div>
+          <h2 className='p-name'></h2>
+          <p className='p-subtext'></p>
+        </div>
+      </div> */}
     </div>
   );
 }
