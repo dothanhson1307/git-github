0c6236698a0a6b0c6847313ab85c3bec165a3f88
thanhson <steaklord1307@gmail.com>
2024-06-19 20:07:27 +0700

    added logic for rankings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './images/ape.png';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect,useState} from 'react';
 import pic1 from './images/coolpic1.jpeg';
 import pic2 from './images/coolpic2.jpeg';
 import pic3 from './images/coolpic3.webp';
@@ -9,22 +9,72 @@ import second from './images/sec.png';
 import third from './images/thir.png';
 function App() {
   useEffect(()=>{
-  function makeTextCool(id){
-      var element = document.getElementById(id);
-      if(element){
-        var text = element.innerText.split("");
-        element.innerText = "";
-        text.forEach((letter) => {
-          var span = document.createElement('span');
-          span.innerText = letter;
-          span.classList.add('letter');
-          element.appendChild(span);
-        })
+    function makeTextCool(id){
+        var element = document.getElementById(id);
+        if(element){
+          var text = element.innerText.split("");
+          element.innerText = "";
+          text.forEach((letter) => {
+            var span = document.createElement('span');
+            span.innerText = letter;
+            span.classList.add('letter');
+            element.appendChild(span);
+          })
+        }
       }
-    }
     makeTextCool('title');
+    function findbest(products){
+      var highest = 0;
+      var name = '';
+      for(let i=0;i<products.length;i++){
+        if(products[i].sold > highest){
+          highest = products[i].sold;
+          name = products[i].name
+        }
+      }
+      setFirstplc(name);
+      for(let i=0;i<products.length;i++){
+        if(products[i].name == name){
+          products.splice(i,1)
+        }
+      }
+      highest = 0;
+      for(let i=0;i<products.length;i++){
+        if(products[i].sold > highest){
+          highest = products[i].sold;
+          name = products[i].name
+        }
+      }
+      setSecondplc(name);
+      console.log('sec',name);
+      for(let i=0;i<products.length;i++){
+        if(products[i].name == name){
+          products.splice(i,1)
+        }
+      }
+      highest = 0;
+      for(let i=0;i<products.length;i++){
+        if(products[i].sold > highest){
+          highest = products[i].sold;
+          name = products[i].name
+        }
+      }
+      setThirdplc(name);
+
     }
-  )
+    findbest(products);
+  },[])
+  const products = [
+    {name:'tigerr',sold:170,rate:-25},
+    {name:'ccaarr',sold:250,rate:50},
+    {name:'sphere',sold:120,rate:70},
+    {name:'window',sold:270,rate:-50},
+    {name:'tablet',sold:5,rate:200},
+    {name:'seizure',sold:130,rate:30},
+  ];
+  const [firstplc,setFirstplc] = useState('');
+  const [secondplc,setSecondplc] = useState('');
+  const [thirdplc,setThirdplc] = useState('');
   return (
     <div className="App">
       <div id='navbar-ctn'>
@@ -46,7 +96,7 @@ function App() {
             </div>
             <div className='textbox-ctn'>
               <div className='textbox'>
-              <h2 className='p-name'>TIGERR</h2>
+              <h2 className='p-name'>{secondplc}</h2>
               <p className='p-subtext'>coolstuff attracts/attacks prey</p>
               <p className='sold'>
                 <p>Sold:</p>
@@ -64,11 +114,11 @@ function App() {
             </div>
             <div className='textbox-ctn'>
               <div className='textbox'>
-              <h2 className='p-name'>CCAARR</h2>
+              <h2 className='p-name'>{firstplc}</h2>
               <p className='p-subtext'>can move at super speed</p>
               <p className='sold'>
                 <p>Sold:</p>
-                <p className='number-s'>170</p>
+                <p className='number-s'>250</p>
               </p>
               </div>
             </div>
@@ -82,16 +132,19 @@ function App() {
             </div>
             <div className='textbox-ctn'>
               <div className='textbox'>
-              <h2 className='p-name'>SPHERE</h2>
+              <h2 className='p-name'>{thirdplc}</h2>
               <p className='p-subtext'>cool, no need explanation</p>
               <p className='sold'>
                 <p>Sold:</p>
-                <p className='number-s'>170</p>
+                <p className='number-s'>120</p>
               </p>
               </div>
             </div>
           </div>
         </div>
+      </div>
+      <div id='products-list'>
+
       </div>
     </div>
   );
